66a00ec55d98e3ad26415aa62420fab672567fc2
Documenter.jl <documenter@juliadocs.github.io>
2025-09-25 16:14:48 +0000

    build based on 2b91b0f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#AstrochemicalYields","page":"Home","title":"AstrochemicalYields","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AstrochemicalYields.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
