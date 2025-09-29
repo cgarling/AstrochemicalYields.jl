b2fe0437acd3eb4a906dea98645291e371c2f5f2
cgarling <chris.t.garling@gmail.com>
2025-09-29 12:40:53 -0400

    Check that data `Z` matches assumed linear range

--- src/Vincenzo2016.jl
+++ src/Vincenzo2016.jl
@@ -4,7 +4,7 @@
 
 """
     Vincenzo2016(; bounds=Throw())
-Interpolator for the stellar lifetime fits of Vincenzo+2016. These are based on PARSEC stellar models. The lifetimes can be interpolated by calling an instance with a metal mass fraction `Z` and a stellar mass `M` (in solar masses), returning the stellar lifetime in Gyr. `bounds` should be a valid `Interpolations.jl` extrapolation specifier that will determine how the interpolation is extrapolated (e.g., `Flat()`).
+Interpolator for the stellar lifetime fits of Vincenzo+2016. These are based on PARSEC stellar models. The lifetimes can be interpolated by calling an instance with a metal mass fraction `Z` and a stellar mass `M` (in solar masses), returning the stellar lifetime in Gyr. The keyword argument `bounds` should be a valid `Interpolations.jl` extrapolation specifier that will determine how the interpolation is extrapolated (e.g., `Flat()`).
 
 ```jldoctest
 julia> using AstrochemicalYields: Vincenzo2016
@@ -36,9 +36,12 @@ function Vincenzo2016(; bounds=Throw())
         table[i,:] .= parse.(Float64, parts[1:4])
     end
     table_vec = [SVector{size(table,2)-1}(table[i,2:end]) for i in 1:size(table,1)]
-    # Z = SVector{size(table, 1)}(table[:,1])
-    # itp = interpolate((Z,), table_vec, Gridded(Linear()))
+    Z_data = SVector{size(table, 1)}(table[:,1])
     Z = 1e-4:1e-4:0.0299
+    if !isapprox(Z, Z_data)
+        error("Vincenzo2016 data malformed.")
+    end
+    # itp = interpolate((Z,), table_vec, Gridded(Linear()))
     itp = scale(interpolate(table_vec, BSpline(Cubic())), Z)
     itp = extrapolate(itp, bounds)
     return Vincenzo2016(itp)
