4063fb491dfca18e720f30955cd4f46b3b8f829c
cgarling <chris.t.garling@gmail.com>
2025-10-24 11:33:03 -0400

    annotate AGB module

--- src/AGB/agbs.jl
+++ src/AGB/agbs.jl
@@ -1,9 +1,8 @@
-
+"""Module containing models for AGB yields."""
 module AGB
 
 using ..AstrochemicalYields: AbstractYield, extend_bounds
 import ..AstrochemicalYields: isotopes, remnant_mass, ejecta_mass, ejecta_metal_mass, ejecta_alpha_mass
-
 
 using Interpolations: interpolate, Throw, Gridded, Linear, extrapolate
 using StaticArrays: SVector
