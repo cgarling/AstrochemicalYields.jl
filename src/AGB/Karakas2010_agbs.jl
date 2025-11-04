9a96530e33015faacbe253228ded4098c62bf0d9
cgarling <chris.t.garling@gmail.com>
2025-11-04 17:15:51 -0500

    Fix Karakas2010 metal yields

--- src/AGB/Karakas2010_agbs.jl
+++ src/AGB/Karakas2010_agbs.jl
@@ -1,3 +1,12 @@
+module Karakas2010
+
+using ...AstrochemicalYields: AbstractYield, extend_bounds
+import ...AstrochemicalYields: isotopes, remnant_mass, ejecta_mass, ejecta_metal_mass, ejecta_alpha_mass
+
+using Interpolations: interpolate, Throw, Gridded, Linear, extrapolate
+using StaticArrays: SVector
+
+export Karakas2010AGB, isotopes, remnant_mass, ejecta_mass, ejecta_metal_mass, ejecta_alpha_mass
 
 const _Karakas2010_Zs = (0.0001, 0.004, 0.008, 0.02)
 const _Karakas2010_M = (1.0, 1.25, 1.5, 1.75, 1.9, 2.0, 2.1, 2.25, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0)
@@ -28,8 +37,8 @@ function Karakas2010AGBEntry(fname)
     # isotopes = Vector{String}(undef, length(lines))
     # M = Vector{Float64}(undef, length(lines))
     # table = Matrix{Float64}(undef, length(parts[1]) - 4, length(M))
-    table = Array{Float64}(undef, length(parts[1]) - 4, length(isotopes), length(M))
-    # table = fill(Inf, (length(parts[1]) - 4, length(isotopes), length(M)))
+    # table = Array{Float64}(undef, length(parts[1]) - 4, length(isotopes), length(M))
+    table = fill(Inf, (length(parts[1]) - 4, length(isotopes), length(M)))
     mask = vcat(3, 6:lastindex(parts[1]))
     for (i, p) in enumerate(parts) # enumerate(lines)
         Mi = parse(Float64, p[1])
@@ -40,6 +49,20 @@ function Karakas2010AGBEntry(fname)
             table[:, 1, j] .= M_final[j]
         end
     end
+    # Capitalize first letter of each element identifier
+    # following convention of other yield tables (e.g., Nomoto)
+    for i in 6:lastindex(isotopes)
+        isotopes[i] = uppercasefirst(isotopes[i])
+    end
+    #"al-6" is the ground state of Al26, "al*6" is the short-lived metastable state
+    # that should be added to Mg26 for chemical evolution studies
+    al26g_idx = findfirst(==("Al-6"), isotopes)
+    al26meta_idx = findfirst(==("Al*6"), isotopes)
+    mg26_idx = findfirst(==("Mg26"), isotopes)
+    # Now add Al*6 yields to Mg26, 2:end because 1 is Mfinal, last is logarithmic production factor
+    # Looks like al*6 yields are always 0 actually?
+    @views table[2:end-1, mg26_idx, :] .+= table[2:end-1, al26meta_idx, :]
+    isotopes[al26g_idx] = "Al26"
     return Karakas2010AGBEntry(Z, M, M_final, table, isotopes)
 end
 
@@ -58,7 +81,7 @@ true
 julia> isapprox(result.Mfinal, 0.852)
 true
 
-julia> isapprox(result.g, 3.5865554e-7)
+julia> isapprox(result.g, 5.6565199e-7)
 true
 
 julia> result2 = k(0.01, 4.75);
@@ -66,7 +89,7 @@ julia> result2 = k(0.01, 4.75);
 julia> isapprox(result2.Mfinal, 0.8721666666666666)
 true
 
-julia> isapprox(result2.p, -0.2422697775)
+julia> isapprox(result2.p, 2.584407875)
 true
 
 julia> remnant_mass(k, 0.01, 4.75) == k(0.01, 4.75).Mfinal
@@ -91,7 +114,7 @@ function Karakas2010AGB(; bounds=Throw())
     # The entries[1].table is 3-D with shape (length(properties), length(isotopes), length(masses))
     # The properties are different forms of the yield / wind properties, for now we only care about the total mass lost in the wind, so that's all we'll interpolate
 
-    iso_mat = [SVector{length(entries[1].isotopes)}(i.table[2, :, j]) for i=entries, j=eachindex(entries[1].M)]
+    iso_mat = [SVector{length(entries[1].isotopes)}(i.table[3, :, j]) for i=entries, j=eachindex(entries[1].M)]
     # The number of model stars (M) at each metallicity is the same coming out of Karakas2010Entry, but the values of the initial stellar masses are not the same -- some metallicities have 2.0 models and some have 2.1. We will pre-interpolate the values so that at all metallicities, model stars with the same masses are available.
     iso_mat = [interpolate((entries[i].M,), iso_mat[i, :], Gridded(Linear()))(m) for i=eachindex(entries), m=_Karakas2010_M]
     # Need to sort entries according to Z, not sorted by default
@@ -111,16 +134,17 @@ end
 remnant_mass(x::Karakas2010AGB, Z, M) = x(Z, M).Mfinal
 ejecta_mass(x::Karakas2010AGB, Z, M) = M - remnant_mass(x, Z, M)
 
-# Don't care to add these right now
-# function filter_metals(nt::NamedTuple)
-#     names = (:Li6, :Li7, :Be9, :B10, :B11, :C12, :C13, :N14, :N15, :O16, :O17, :O18, :F19, :Ne20, :Ne21, :Ne22, :Na23, :Mg24, :Mg25, :Mg26, :Al26, :Al27, :Si28, :Si29, :Si30, :P31, :S32, :S33, :S34, :S36, :Cl35, :Cl37, :Ar36, :Ar38, :Ar40, :K39, :K40, :K41, :Ca40, :Ca42, :Ca43, :Ca44, :Ca46, :Ca48, :Sc45, :Ti46, :Ti47, :Ti48, :Ti49, :Ti50, :V50, :V51, :Cr50, :Cr52, :Cr53, :Cr54, :Mn55, :Fe54, :Fe56, :Fe57, :Fe58, :Co59, :Ni58, :Ni60, :Ni61, :Ni62, :Ni64, :Cu63, :Cu65, :Zn64, :Zn66, :Zn67, :Zn68, :Zn70, :Ga69, :Ga71)
-#     return nt[names]
-# end
-# ejecta_metal_mass(x::Nomoto2006SN, Z, M) = sum(values(filter_metals(x(Z, M))))
+function filter_metals(nt::NamedTuple)
+    names = (:Li7, :Be7, :B8, :C12, :C13, :C14, :N13, :N14, :N15, :O14, :O15, :O16, :O17, :O18, :O19, :F17, :F18, :F19, :F20, :Ne19, :Ne20, :Ne21, :Ne22, :Na23, :Na24, :Mg23, :Mg24, :Mg25, :Mg26, :Mg27, :Al25, :Al26, :Al27, :Al28, :Si27, :Si28, :Si29, :Si30, :Si31, :Si32, :Si33, :P29, :P30, :P31, :P32, :P33, :P34, :S32, :S33, :S34, :S35, :Fe54, :Fe55, :Fe56, :Fe57, :Fe58, :Fe59, :Fe60, :Fe61, :Co59, :Co60, :Co61, :Ni58, :Ni59, :Ni60, :Ni61, :Ni62)
+    return nt[names]
+end
+ejecta_metal_mass(x::Karakas2010AGB, Z, M) = sum(values(filter_metals(x(Z, M))))
 
-# # Filter to include only alpha elements
-# function filter_alpha(nt::NamedTuple)
-#     names = (:O16, :O17, :O18, :Ne20, :Ne21, :Ne22, :Mg24, :Mg25, :Mg26, :Si28, :Si29, :Si30, :S32, :S33, :S34, :S36, :Ar36, :Ar38, :Ar40, :Ca40, :Ca42, :Ca43, :Ca44, :Ca46, :Ca48, :Ti46, :Ti47, :Ti48, :Ti49, :Ti50)
-#     return nt[names]
-# end
-# ejecta_alpha_mass(x::Nomoto2006SN, Z, M) = sum(values(filter_alpha(x(Z, M))))
+# Filter to include only alpha elements
+function filter_alpha(nt::NamedTuple)
+    names = (:O14, :O15, :O16, :O17, :O18, :O19, :Ne19, :Ne20, :Ne21, :Ne22, :Mg23, :Mg24, :Mg25, :Mg26, :Mg27, :Si27, :Si28, :Si29, :Si30, :Si31, :Si32, :Si33, :S32, :S33, :S34, :S35)
+    return nt[names]
+end
+ejecta_alpha_mass(x::Karakas2010AGB, Z, M) = sum(values(filter_alpha(x(Z, M))))
+
+end # module
--- src/AGB/agbs.jl
+++ src/AGB/agbs.jl
@@ -4,10 +4,8 @@ module AGB
 using ..AstrochemicalYields: AbstractYield, extend_bounds
 import ..AstrochemicalYields: isotopes, remnant_mass, ejecta_mass, ejecta_metal_mass, ejecta_alpha_mass
 
-using Interpolations: interpolate, Throw, Gridded, Linear, extrapolate
-using StaticArrays: SVector
-
 include("Karakas2010_agbs.jl")
+using .Karakas2010
 
 export Karakas2010AGB
 
