ac10f810dccc5fcbb32ce260c0c5bd87469ea3b0
cgarling <chris.t.garling@gmail.com>
2025-11-05 08:07:10 -0500

    try adding `init_code`

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using ParallelTestRunner: runtests
 
 # Can run single test files with, e.g., Pkg.test("AstrochemicalYields"; test_args=`--verbose doctests`)
 # Can list available single tests with Pkg.test("AstrochemicalYields"; test_args=`--list`);
-runtests(AstrochemicalYields, Base.ARGS) # ; init_code)
+const init_code = quote
+    using AstrochemicalYields
+end
+runtests(AstrochemicalYields, Base.ARGS; init_code)
