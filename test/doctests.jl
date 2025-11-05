ed81eb7a9944b1547572b541b64ef84372482cd7
cgarling <chris.t.garling@gmail.com>
2025-11-05 07:46:40 -0500

    try not using eval in doctest

--- test/doctests.jl
+++ test/doctests.jl
@@ -1,6 +1,11 @@
 # Need to load AstrochemicalYields into Main to work with ParallelTestRunner
-@eval Main using AstrochemicalYields
-using Documenter: DocMeta, doctest
+# @eval Main using AstrochemicalYields
+# using Documenter: DocMeta, doctest
 
-DocMeta.setdocmeta!(Main.AstrochemicalYields, :DocTestSetup, :(using AstrochemicalYields); recursive=true)
-doctest(Main.AstrochemicalYields)
+# DocMeta.setdocmeta!(Main.AstrochemicalYields, :DocTestSetup, :(using AstrochemicalYields); recursive=true)
+# doctest(Main.AstrochemicalYields)
+
+using AstrochemicalYields
+using Documenter: DocMeta, doctest
+DocMeta.setdocmeta!(AstrochemicalYields, :DocTestSetup, :(using AstrochemicalYields); recursive=true)
+doctest(AstrochemicalYields)
